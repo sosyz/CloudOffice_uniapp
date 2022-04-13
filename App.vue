fa0fda2cf29d724b0e9acc578cb1f01ac76795fa
Sonui <me@sonui.cn>
2022-04-13 23:47:40 +0800

    更改登录行为及路径

--- App.vue
+++ App.vue
@@ -2,9 +2,20 @@
 import utils from './js_sdk/utils.js'
 export default {
 	onLaunch: function() {
-		let openid = uni.getStorageSync('openid');
-		if (!openid) openid = utils.updateOpenId();
-		utils.updateTmpKey();
+		// #ifdef MP-WEIXIN
+		wx.checkSession({
+		  success () {
+		    //session_key 未过期，并且在本生命周期一直有效	
+			utils.updateTmpKey();
+		  },
+		  fail () {
+		    // session_key 已经失效，需要重新执行登录流程
+		    utils.login();
+			utils.updateTmpKey();
+		  }
+		})
+		// #endif
+		
 	},
 	onShow: function() {
 		
--- js_sdk/utils.js
+++ js_sdk/utils.js
@@ -1,7 +1,16 @@
 import config from './config'
 
+/**
+ * @param {Object} path 文件本地路径
+ * @param {Object} name 文件名
+ * @param {Object} callback 回调函数
+ */
 function uploadFile(path, name, callback) {
+	let tmpKey = getApp().tmpKey;
 	const currentDate = new Date();
+	if (tmpKey.ExpiredTime >= currentDate.getTime() / 1000) {
+		login()
+	}
 	const expirationDate = new Date(currentDate.getTime() + expires * 1000);
 	const keyTime = `${Math.floor(currentDate.getTime() / 1000)};${Math.floor(expirationDate.getTime() / 1000)}`;
 	const policy = JSON.stringify({
@@ -25,63 +34,87 @@ function uploadFile(path, name, callback) {
 		signature,
 		policy: Buffer.from(policy).toString('base64')
 	});
-}
+	//callback;
 
+}
 
 /**
  * 更新临时密钥
  * @param {Object} openid
  */
-function updateTmpKey(openid) {
+function updateTmpKey() {
+	let openid = uni.getStorageSync("openid");
+	let session = uni.getStorageSync("session");
 	uni.request({
-		url: 'http://192.168.3.210:8080/user/' + openid + '/getTmpKey',
+		url: 'http://192.168.3.210:8080/user/getTmpKey',
+		method: 'POST',
+		data: {
+			session: session,
+			openid: openid,
+		},
 		success: function(result) {
 			getApp().tmpKey = result.data;
 			console.log(getApp().tmpKey);
 		}
 	})
 }
+
 /**
- * 更新用户标识
+ * 登录
  */
-function updateOpenId() {
-	let openid = uni.getStorageSync('openid');
-	if (openid != "") return openid;
-	// #ifdef MP-WEIXIN
-	wx.login({
-		success(res) {
-			if (res.code) {
-				//发起网络请求
-				wx.request({
-					url: 'http://192.168.3.210:8080/login',
-					method: 'POST',
-					data: {
-						code: res.code
-					},
-					success(res2) {
-						if (res2.data.code != 0) {
-							console.log('服务器登录失败' + res2.data.msg);
-						} else {
-							openid = res2.data.openid;
-							uni.setStorage({
-								key: "openid",
-								data: openid,
-								success() {
-									console.log('登录完成，存储值' + openid);
-								}
-							})
+function login() {
+	return new Promise((resolve, reject) => {
+		let openid = uni.getStorageSync('openid');
+		if (openid != "") return openid;
+		// #ifdef MP-WEIXIN
+		wx.login({
+			success(res) {
+				if (res.code) {
+					//发起网络请求
+					wx.request({
+						url: 'http://192.168.3.210:8080/user/login',
+						method: 'POST',
+						data: {
+							code: res.code
+						},
+						success(res2) {
+							if (res2.data.code != 0) {
+								console.log('服务器登录失败' + res2.data.msg);
+							} else {
+								console.log(res2.data);
+								let openid = res2.data.openid;
+								let session = res2.data.session;
+								uni.setStorage({
+									key: "session",
+									data: session,
+									success() {
+										console.log('登录完成，session: ' + session);
+									}
+								});
+								uni.setStorage({
+									key: "openid",
+									data: openid,
+									success() {
+										console.log('登录完成，openid: ' + openid);
+									}
+								});
+								resolve({
+									openid: uni.getStorageSync('openid'),
+									session: uni.getStorageSync('session')
+								})
+							}
 						}
-					}
-				})
-			} else {
-				console.log('登录失败！' + res.errMsg)
+					})
+				} else {
+					console.log('登录失败！' + res.errMsg)
+				}
 			}
-		}
+		})
+		// #endif
 	})
-	// #endif
-	return uni.getStorageSync('openid');
 }
 export default {
-	updateOpenId,
+	login,
 	updateTmpKey,
+	uploadFile,
 }
